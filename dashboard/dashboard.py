e47018d79a9ed298d31cb380629700cc59744ad2
cnadiaa <c14240030@john.petra.ac.id>
2026-07-28 21:03:36 +0700

    try 1200px

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -13,7 +13,7 @@ st.set_page_config(
 st.markdown("""
 <style>
 .block-container {
-    max-width: 730px;
+    max-width: 1200px;
     margin: auto;
     padding-left: 2rem;
     padding-right: 2rem;
